b2f944d7612a52da9894e07032ed81fa167fb3fb
Shadrack Nicholas <berushadrack@gmail.com>
2026-05-17 15:35:31 +0200

    feat: add versioning for the builds

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,7 +9,20 @@ import (
 	"github.com/shadracnicholas/better-hn/internal/ui"
 )
 
+// version is set at build time via:
+//
+//	go build -ldflags "-X main.version=1.2.3"
+var version = "dev"
+
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println("better-hn", version)
+		return
+	}
+
 	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
